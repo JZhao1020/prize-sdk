4d8ac9f03869c40e55fe3b0615db62485b506cc2
PC-20160730MLQF\Administrator <18816784184@163.com>
2018-05-31 11:18:53 +0800

    新增README说明、调整方法为静态方法

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# prize
-处理抽奖逻辑
+# 自定义抽奖概率计算
+
+## 开源地址
+https://github.com/JZhao1020/prize-sdk
+
+##1.安装
+```
+composer require hao/prize-sdk 
+```
+
+##2.示例
+```
+$arr = [
+   ['id' => 1,'prize' => '一等奖', 'v' => 1],
+   ['id' => 2,'prize' => '二等奖', 'v' => 1],
+   ['id' => 3,'prize' => '三等奖', 'v' => 100],
+   ['id' => 4,'prize' => '谢谢参与', 'v' => 1000],
+];
+
+$result = \Prize\Prize::init($arr);
+dump($result);
+```
--- src/Prize.php
+++ src/Prize.php
@@ -27,7 +27,7 @@ class Prize{
      * @param array $arr
      * @return int|false
      */
-    private function get_rand($arr){
+    private static function get_rand($arr){
         //获取奖项的总概率
         $probability = array_sum($arr);
         if($probability == 0){
@@ -51,7 +51,7 @@ class Prize{
      * @param array $field 数据中对应的键名数组
      * @return array|false
      */
-    public function init(array $data, $field = array('id','prize','v')){
+    public static function init(array $data, $field = array('id','prize','v')){
         if(count($data) <= 0) {
             return false;
         }
@@ -59,9 +59,9 @@ class Prize{
         //数据重组成一维数组，随机返回获取奖项id
         $prize = array();
         foreach ($data as $key => $val){
-            $prize[$val[$field[0]]] = $val[$field[2]];
+            $prize[$key] = $val[$field[2]];
         }
-        $prize_id = $this->get_rand($prize);
+        $prize_id = self::get_rand($prize);
         $result = array(
             $field[0] => $data[$prize_id][$field[0]],
             $field[1] => $data[$prize_id][$field[1]],
